bfc4156cfa2d0d67cc51d149cae5491e7d1c9188
salmanfxrsi <salmanxprivate@gmail.com>
2024-12-15 14:41:28 +0600

    feat: add job posting functionality
    - Implemented  endpoint to save job data to the database.
    - Integrated job posting form in the  component.
    - Collected and structured form data, including title, buyer details, deadline, category, price range, and description.
    - Utilized Axios for sending POST requests to the backend.
    - Added date handling with  for deadline selection.

--- server/index.js
+++ server/index.js
@@ -9,9 +9,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.yolij.mongodb.net/?retryWrites=true&w=majority&appName=Main`
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.yolij.mongodb.net/?retryWrites=true&w=majority&appName=Main`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.upkox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,15 +21,23 @@ const client = new MongoClient(uri, {
   },
 })
 
+const jobsCollection = client.db('jobsDB').collection('jobs') 
+
 async function run() {
   try {
-    // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+
+    // post job 
+    app.post('/add-job', async(req,res)=>{
+      const jobData = req.body;
+      const result = await jobsCollection.insertOne(jobData);
+      res.send(result);
+    })
+
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     )
   } finally {
-    // Ensures that the client will close when you finish/error
+    
   }
 }
 run().catch(console.dir)
